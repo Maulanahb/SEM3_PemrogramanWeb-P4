5c945467ba7eaee5792cf795c776fd9432eb5786
Maulanahb <maulanahmad2287@gmail.com>
2025-09-15 20:20:16 +0700

    Add calculations and output for average scores in tipe_data.php

--- tipe_data.php
+++ tipe_data.php
@@ -10,5 +10,19 @@ echo "Variable b : {$b} <br>";
 echo "Variable c : {$c} <br>";
 echo "Variable d : {$d} <br>";
 echo "Variable e : {$e} <br>";
-
 var_dump($e);
+echo"<br>";
+
+$nilaiMatematika =5.1;
+$nilaiIPA =6.7;
+$nilaiBahasaIndonesia=9.3;
+
+$rataRata =($nilaiMatematika +  $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika : {$nilaiMatematika} <br>";
+echo "IPA : {$nilaiIPA} <br>";
+echo "Bahasa Indonesia : {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata : {$rataRata} <br>";
+
+var_dump($rataRata);
+?>
